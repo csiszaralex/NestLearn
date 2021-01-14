29b7fec16a238508b7f91baf8168ad48bed60304
Alex <csiszaralex@gmail.com>
2021-01-14 23:59:08 +0100

    :passport_control: Add JWT to authenticate

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,9 +1,6 @@
-import {
-  ConflictException,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
@@ -19,11 +16,19 @@ export class UserRepository extends Repository<User> {
     try {
       await user.save();
     } catch (err) {
-      if (err.code === '23505')
-        throw new ConflictException('Username already exists');
+      if (err.code === '23505') throw new ConflictException('Username already exists');
       else throw new InternalServerErrorException();
     }
   }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) return user.username;
+    else return null;
+  }
+
   private async hashPassword(password: string, salt: string): Promise<string> {
     return bcrypt.hash(password, salt);
   }
